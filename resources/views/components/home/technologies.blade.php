702b09fe9ec487bf0348789a6742f8b2d8f8dd73
Simon Bashir <itproserve@gmail.com>
2021-07-25 22:52:30 -0500

    content update for the home page

--- resources/views/components/home/technologies.blade.php
+++ resources/views/components/home/technologies.blade.php
@@ -5,7 +5,9 @@
 <!-- OUTER WRAPPER -->
 <div class="w-full flex flex-col justify-center py-4 xl:py-8">
 
- <x-home.banner svg="technologies">Technologies</x-home.banner>
+  <x-home.banner svg="technologies">Technologies</x-home.banner>
+
+  <h3 class="mt-12 text-xl md:text-2xl xl:text-3xl text-gray-700 mx-auto px-2 xl:px-4">Job Save was developed using the following technology stack and tools</h3>
 
   <div class="container mx-auto mt-2 w-full py-24 px-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2">
     <a href="https://laravel.com/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="Laravel">
