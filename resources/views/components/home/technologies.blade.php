b2702c6baca90bc36ded5a4287255bbd8f90ae74
Simon Bashir <itproserve@gmail.com>
2021-07-25 23:00:00 -0500

    content update for the Technologies section of the Home page

--- resources/views/components/home/technologies.blade.php
+++ resources/views/components/home/technologies.blade.php
@@ -9,12 +9,12 @@
 
   <h3 class="mt-12 text-xl md:text-2xl xl:text-3xl text-gray-700 mx-auto px-2 xl:px-4">Job Save was developed using the following technology stack and tools</h3>
 
-  <div class="container mx-auto mt-2 w-full py-24 px-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2">
+  <div class="container mx-auto mt-2 w-full py-12 px-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-2">
     <a href="https://laravel.com/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="Laravel">
       <x-svg.technologies type="laravel" />
     </a>
-    <a href="https://php.net/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="PHP">
-      <x-svg.technologies type="php" />
+    <a href="https://vuejs.org/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="VueJS">
+      <x-svg.technologies type="vue" />
     </a>
     <a href="https://mysql.com/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="MySQL">
       <x-svg.technologies type="mysql" />
@@ -30,9 +30,6 @@
     </a>
     <a href="https://www.digitalocean.com/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="Digital Ocean">
       <x-svg.technologies type="digitalocean" />
-    </a>
-    <a href="https://vuejs.org/" class="w-32 h-32 mb-12 md:mb-0 border border-gray-200 flex items-center justify-center p-2" title="VueJS">
-      <x-svg.technologies type="vue" />
     </a>
   </div> <!-- TECHNOLOGIES -->
 </div>
